006f78f0c3072d06a77b66a09f0451d540d018af
Samuel <samfrank41802@gmail.com>
2023-10-22 19:04:35 -0400

    Update E-Board and styled the resource buttons

--- src/assets/Components/EBoardComponent/index.data.jsx
+++ src/assets/Components/EBoardComponent/index.data.jsx
@@ -1,69 +1,82 @@
-import Minghui from '../../images/E-board/minghui.jpg'; // president
-import Sam from '../../images/E-board/sam.png'; // vice-president
-import Zach from '../../images/E-board/zach.jpg'; // tutoring chair
-import Ginger from '../../images/E-board/ginger.jpeg'; // event chair
-import Matt from '../../images/E-board/matt.jpg'; // internal communication chair
-import Judy from '../../images/E-board/judy.jpg'; // business manager
-import Fiona from '../../images/E-board/fiona.png'; // DandyHacks chair
-import Emily from '../../images/E-board/emily.jpg'; // social media manager
-// import Duy from '../../images/E-board/duy.jpeg'; // first-year rep
+import samFrank from '../../images/E-board/2023-2024/Sam_Frank.png'; // president
+import zachNguyen from '../../images/E-board/2023-2024/Zach.png'; // vice-president
+import gigiPan from '../../images/E-board/2023-2024/Gigi.JPG'; // tutoring chair
+import richardChuong from '../../images/E-board/2023-2024/richard.jpg'; // event chair
+import katieSeo from '../../images/E-board/2023-2024/Katie.jpg'; // internal communication chair
+import kevinWang from '../../images/E-board/2023-2024/Kevin.png'; // business manager
+import gingerLi from '../../images/E-board/2023-2024/Ginger.jpeg'; // DandyHacks chair
+import tiffanyXiang from '../../images/E-board/2023-2024/tiff.png'; // social media manager
+import sungjaeBae from '../../images/E-board/2023-2024/SJ.JPG'
+import rusmiyaAqid from '../../images/E-board/2023-2024/rusmiya.png'; // first-year rep
 
 
 const eboardmembers = [
     {
         id: 0,
-        name: 'Minghui Zheng',
+        name: 'Samuel Frank',
         title:'President',
-        img: Minghui,
-        gitlink: 'https://github.com/eeeemily',
-        linkedInlink: 'https://www.linkedin.com/in/minghui-zheng/',
+        img: samFrank,
+        gitlink: 'https://github.com/samfrank3',
+        linkedInlink: 'https://www.linkedin.com/in/samuel-n-frank/',
     },
     {
         id: 1,
-        name: 'Sam Frank',
+        name: 'Duong (Zach) Nguyen',
         title:'Vice President',
-        img: Sam,
-        gitlink: 'https://github.com/samfrank3',
-        linkedInlink: 'http://linkedin.com/in/sam-frank-bba69b1b5',
+        img: zachNguyen,
+        gitlink: 'https://github.com/cszach',
+        linkedInlink: 'https://www.linkedin.com/in/zach-nguyen/',
     },
     {
         id: 2,
-        name: 'Zach',
+        name: 'Gigi Pan',
         title:'Tutoring Chair',
-        img: Zach,
-        gitlink: 'https://github.com/cszach',
-        linkedInlink: 'https://www.linkedin.com/in/zach-nguyen/',
+        img: gigiPan,
+        gitlink: '',
+        linkedInlink: '',
 
     },
     {
         id: 3,
-        name: 'Ginger',
+        name: 'Richard Chuong',
         title:'Event Chair',
-        img: Ginger,
+        img: richardChuong,
     },
     {
         id: 4,
-        name: 'Matt Fortes',
+        name: 'Katie Seo',
         title:'Internal Communication Chair',
-        img: Matt,
+        img: katieSeo,
     },
     {
         id: 5,
-        name: 'Judy',
+        name: 'Kevin Wang',
         title:'Business Manager',
-        img: Judy,
+        img: kevinWang,
     },
     {
         id: 6,
-        name: 'Fiona',
+        name: 'Ginger Li',
         title:'DandyHacks Chair',
-        img: Fiona,
+        img: gingerLi,
     },
     {
         id: 7,
-        name: 'Emily',
+        name: 'Tiffany Xiang',
         title:'Social Media Manager',
-        img: Emily,
+        img: tiffanyXiang,
+    },
+    {
+        id: 8,
+        name: 'Sung Jae Bae',
+        title: 'RocLab Director',
+        img: sungjaeBae
+    },
+    {
+        id: 9,
+        name: 'Rusmiya Aqid',
+        title: 'First-Year Representative',
+        img: rusmiyaAqid
     }
 ]
 
